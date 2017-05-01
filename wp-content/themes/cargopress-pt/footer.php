c378b657cf8b3741ed344935ddc20e40548a4b13
info-rasul <rasul.info@gmail.com>
2017-05-02 00:35:24 +0300

    Отзывы

--- wp-content/themes/cargopress-pt/footer.php
+++ wp-content/themes/cargopress-pt/footer.php
@@ -8,20 +8,39 @@
 $footer_widgets_layout = CargoPressHelpers::footer_widgets_layout_array();
 
 ?>
-	<div class="container">
-		<div class="row">
-		<?php echo do_shortcode('[simple_testimonials adaptiveheight="true" arrows="false" dots="true" fade="true" infinite="true" slidestoshow="3" total_post="6" layout="grid" speed="300" autoplayspeed="3000"  autoplay="false"]'); ?>
-		</div>
+	<?php if ( is_front_page()) : ?>
+	<div class="footer-top-predl">
+			<div class="row">
+				<div class="col-md-5"></div>
+				<div class="col-md-7">
+					<div class="hentry__content">
+						<span class="predl-zag">УНИКАЛЬНОЕ ПРЕДЛОЖЕНИЕ! AE STRONG CERAMIC COAT/2С</span>
+						<span class="predl-txt">Профессиональная полировка кузова + нанесение<br> керамического 2х компонентного состава = 45000 рублей! +<br> покрытие всех стекол в подарок и комплексная уборка салона!<br> Встречай весну вместе с нами!</span>
+					</div>
+				</div>
+			</div>
 	</div>
 
-	<?php if ( is_front_page()) : ?>
+	<div class="container">
+			<div class="head_block row otziv">
+			<span class="line-top-otz"></span>
+			<span class="head-text">Отзывы</span>
+		</div>
+
+		<div class="row">
+		<?php echo do_shortcode('[simple_testimonials adaptiveheight="true" arrows="false" dots="true" fade="true" infinite="true" slidestoshow="3" total_post="6" layout="grid" speed="300" autoplayspeed="3000"  autoplay="false"]'); ?>
+			<div class="head_block row" style="margin-top: 40px; margin-bottom: 0px;">
+				<span class="line-bottom"></span>
+			</div>
+		</div>
+	</div>
 	<span class="line-razd"></span>
 	<div class="footer-logos">
 		<div class="head_block row">
 			<span class="head-text">Партнеры</span>
 			<span class="line-bottom"></span>
 		</div>
-			<?php echo do_shortcode('[logoshowcase center_mode="true" slides_column="6" autoplay="false" dots="false" center_mode="false" ]'); ?>
+		<?php echo do_shortcode('[logoshowcase center_mode="true" slides_column="6" autoplay="false" dots="false" center_mode="false" ]'); ?>
 	</div>
 	<?php endif ?>
 
